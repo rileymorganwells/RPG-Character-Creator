a3150a262f0b1a6eb5b093979a9b23bc886ea0ea
Andrew Marquez <andy2mgcc@gmail.com>
2018-11-15 23:53:47 -0700

    Fixed logic

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -15,9 +15,7 @@ function MainCtrl ($scope, $http) {
     $scope.formContent='';
     $http.post('/characters', newcharacter)
     .success(function(data) {
-      if (window.location != '/') {
-        window.location = '/';
-      }
+      window.location = '/';
       $scope.characters.push(data);
     });
   };
