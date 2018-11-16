c78ed29141e89d40c2bb8abe52b6acf3d193eace
rileymorganwells@gmail.com <ec2-user@ip-172-31-6-65.us-west-2.compute.internal>
2018-11-16 04:36:26 +0000

    Added several method to routes to index.js, added characters and create html pages

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,28 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Character = mongoose.model('Character');
+var path = require('path');
 
 router.get('/characters', function(req, res, next) {
   Character.find(function(err, characters){
     if(err){ return next(err); }
     res.json(characters);
+  });
+});
+
+router.get('/pickCharacter', function(req, res, next) {
+  res.sendFile(path.resolve('public/characters.html'));
+});
+
+router.get('/create/:charID', function(req, res, next) {
+  res.sendFile(path.resolve('public/create.html'));
+});
+
+router.post('/create/:charID?', function(req, res, next) {
+  var character = new Character(req.body);
+  character.save(function(err, comment) {
+    if(err){ return next(err); }
+    res.sendFile(path.resolve('public/create.html'));
   });
 });
 
