73d70173e94e5af7d1673e5db11e3fd6101c060e
Andrew Marquez <andy2mgcc@gmail.com>
2018-11-15 19:55:09 -0700

    Removed jQuery dependencies and added deleteCharacters to Angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,31 +1,33 @@
 angular.module('character', [])
 .controller('MainCtrl', [
   '$scope','$http',
-  function($scope,$http){
+  function ($scope, $http) {
     $scope.characters = [];
+
     $scope.addCharacter = function() {
       var newcharacter = {characterName:$scope.name,characterImage:$scope.image,stats:0};
       $scope.formContent='';
-      $http.post('/characters', newcharacter).success(function(data){
+      $http.post('/characters', newcharacter)
+      .success(function(data) {
         $scope.characters.push(data);
       });
     };
-/*    $scope.upvote = function(comment) {
-      return $http.put('/comments/' + comment._id + '/upvote')
-        .success(function(data){
-          console.log("upvote worked");
-          comment.upvotes = data.upvotes;
-        });
-    };
-	$scope.incrementUpvotes = function(comment) {
-	  $scope.upvote(comment);
-    };*/
+
+    $scope.deleteCharacters = function() {
+      $http.delete('/characters')
+      .success(function(data) {
+        $scope.getAll();
+        console.log("In deletion success");
+      });
+    }
+
     $scope.getAll = function() {
       return $http.get('/characters').success(function(data){
         angular.copy(data, $scope.characters);
       });
     };
-    $scope.getAll();
 
+    // On page load
+    $scope.getAll();
   }
 ]);
